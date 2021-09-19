b3f5c2c96e293e6776c62e1074a61ee7bdc466bf
GuilhermePCoutinho <guilherme_pc@outlook.com>
2021-09-19 14:32:09 -0300

    adicionado JavaScript no formulário junto com validações e mais algumas alterações no layout

---
+++ src/Controllers/UserController.js
@@ -0,0 +1,51 @@
+class UserController {
+    constructor(formid) {
+
+        this.form = document.getElementById(formid);
+        this.onSubmit();
+    }
+
+    onSubmit() {
+        this.form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            this.getValues();
+        })
+    }
+
+    message(user) {
+        console.log(user);
+    }
+
+    validation(element) {
+        if (["email", "password"].indexOf(element.name) > -1 && !element.value) {
+            element.classList.remove("success")
+            element.classList.add("error")
+            return false;
+        } else {
+            element.classList.remove("error")
+            element.classList.add("success")
+
+        }
+
+    }
+
+    getValues() {
+        let user = {};
+        let isvalid = true;
+
+
+        [...this.form.elements].forEach((element, index) => {
+            isvalid = this.validation(element);
+
+            if (element.name) {
+                user[element.name] = element.value;
+            }
+        })
+        if (!isvalid) {
+            return false;
+        }
+        let object = new User(user.email, user.password);
+
+        this.message(object);
+    }
+}
